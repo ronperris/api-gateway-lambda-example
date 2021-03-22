8314b86abc6081ed357f92563a8dcadf5c0b7b28
Ron Perris <ron@perr.is>
2021-03-21 19:38:46 -0600

    Added cognito pool settings.

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "us-west-2_sgXkva3hH", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "6ej2gmalnhks381n1r7uns5sb5", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-west-2", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
