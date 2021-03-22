502757148138d39789e89423fe4d10c313990ed0
Ron Perris <ron@perr.is>
2021-03-22 12:53:43 -0600

    Linked a different cognito pool.

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
   cognito: {
-    userPoolId: "us-west-2_sgXkva3hH", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "6ej2gmalnhks381n1r7uns5sb5", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    userPoolId: "us-west-2_yMefaHxzV", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "6sejutaugjch01upifro2ua1g6", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
     region: "us-west-2", // e.g. us-east-2
   },
   api: {
